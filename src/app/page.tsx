1b2a6a328d7df7099267de33d9e6f50fa5a3219b
Lucas Ramos Marques <lucas@rdweb.com.br>
2023-05-12 18:06:06 -0300

    feat: ajuste mobiles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,15 +102,15 @@ export default function Home() {
 
           <main>
             {/* BOX PRINCIPAL */}
-            <div className='flex justify-center align-middle items-center justify-around mt-24'>
-              <div className='flex align-middle justify-center h-32 flex-col'>
-                <h1 className='text-6xl font-bold'>
+            <div className='flex align-middle items-center flex-col-reverse md:flex-row justify-center md:justify-around mt-12 md:mt-24'>
+              <div className='flex align-middle justify-center items-center md:items-start h-32 flex-col'>
+                <h1 className='mt-60 md:mt-0 text-3xl md:text-6xl font-bold text-center md:text-start'>
                   Ótimo software é<br/>
                   construído com <span className='text-[#EF0178]'> incrível<br/>
                   desenvolvedores</span>
                 </h1>
 
-                <p className='mt-10 text-xl'>Ajudamos a construir e gerenciar uma equipe de desenvolvedores <br/>de classe mundial para dar vida à sua visão</p>
+                <p className='mt-10 text-md md:text-xl text-center md:text-start'>Ajudamos a construir e gerenciar uma equipe de desenvolvedores <br/>de classe mundial para dar vida à sua visão</p>
 
                 <button className='bg-[#5243C2] text-[#fff] font-semibold w-56 p-3 rounded-md mt-10'>Vamos Conversar!</button>
               </div>
@@ -121,25 +121,25 @@ export default function Home() {
               />
             </div>
             {/* BOX SECUNDÁRIO */}
-            <div className='flex justify-center flex-col align-middle items-center mt-16'>
+            <div className='flex justify-center flex-col align-middle items-center mt-64'>
               <h3 className='font-bold text-[#EF0178] text-xl'>Conheça nossos Clientes</h3>
               <p className='font-light text-3xl text-center mt-6'>Esses caras estão usando nosso serviço e estão<br/>mais do que felizes.</p>
-              <ul className='list-none flex align-middle justify-center mt-6'>
-                  <li className='inline p-9 flex align-middle items-center'><img src='/image 11.png' width={100}/></li>
-                  <li className='inline p-9'><img src='/image 12.png' width={130}/></li>
-                  <li className='inline p-9'><img src='/image 13.png' width={150}/></li>
-                  <li className='inline p-9'><img src='/image 14.png' width={150}/></li>
-                  <li className='inline p-9 '><img src='/image 15.png' width={150}/></li>
+              <ul className='list-none flex flex-col md:flex-row align-middle justify-center mt-6'>
+                  <li className='inline p-9 flex justify-center md:justify-normal align-middle items-center'><img src='/image 11.png' width={100}/></li>
+                  <li className='inline p-9 flex justify-center md:justify-normal align-middle items-center'><img src='/image 12.png' width={130}/></li>
+                  <li className='inline p-9 flex justify-center md:justify-normal align-middle items-center'><img src='/image 13.png' width={150}/></li>
+                  <li className='inline p-9 flex justify-center md:justify-normal align-middle items-center'><img src='/image 14.png' width={150}/></li>
+                  <li className='inline p-9 flex justify-center md:justify-normal align-middle items-center'><img src='/image 15.png' width={150}/></li>
               </ul>
             </div>
 
             {/* SOBRE NÓS */}
-            <div className='flex justify-evenly align-middle items-center mt-16'>
+            <div className='flex flex-col-reverse md:flex-row justify-center md:justify-evenly align-middle items-center mt-16'>
               <img src='/img.png' alt='Desenvolvedor no Foguete' width={633}/>
               <div>
-                <h1 className='text-5xl font-bold'>Impulsione suas agências<br/>escolhendo <span className='text-[#5243C2]'>Ninja<br/>Desenvolvedores</span></h1>
-                <p className='mt-5 text-lg'>Venha criar suas landing pages personalizadas<br/>que convertem mais visitantes<br/>do que qualquer site.</p>
-                <ul className='mt-5'>
+                <h1 className='text-center md:text-start text-3xl md:text-5xl font-bold'>Impulsione suas agências<br/>escolhendo <span className='text-[#5243C2]'>Ninja<br/>Desenvolvedores</span></h1>
+                <p className='mt-5 text-center md:text-start text-md md:text-lg'>Venha criar suas landing pages personalizadas<br/>que convertem mais visitantes<br/>do que qualquer site.</p>
+                <ul className='mt-5 flex md:flex-none justify-center md:justify-normal flex-col md:flex-col items-center md:items-start'>
                   <li className='flex align-middle items-center'><img src='/ok.png' alt='Verificado' width={15} className='mr-1'/>Possibilidade de design ilimitada</li>
                   <li className='flex align-middle items-center'><img src='/ok.png' alt='Verificado' width={15} className='mr-1'/>Recursos totalmente responsivos</li>
                   <li className='flex align-middle items-center'><img src='/ok.png' alt='Verificado' width={15} className='mr-1'/>Fácil de personalizar plug-ins</li>
@@ -151,7 +151,7 @@ export default function Home() {
             <div className='flex justify-center flex-col align-middle items-center mt-16'>
               <h3 className='font-bold text-[#EF0178] text-xl'>Nossos Serviços</h3>
               <p className='font-bold text-3xl text-center mt-6'>Determine o serviço que você precisa</p>
-              <ul className='list-none flex align-middle justify-evenly w-auto mt-6'>
+              <ul className='list-none flex flex-col md:flex-row align-middle justify-evenly w-auto mt-6'>
                   <li className='inline p-9 flex justify-center flex-col items-center text-center'>
                     <img src='/frame.png' width={110}/>
                     <h4 className='font-bold text-lg mt-5'>Desenvolvimento</h4>
@@ -176,19 +176,19 @@ export default function Home() {
             </div>
 
             {/* ENTRE EM CONTATO */}
-            <div className='bg-[#5243C2] rounded-xl h-72 flex items-center align-middle justify-center mt-16'>
-              <div className='flex items-center w-[100%] justify-around align-middle'>
+            <div className='bg-[#5243C2] rounded-xl h-72 flex items-center align-middle justify-center mt-8 md:mt-16'>
+              <div className='flex items-center flex-col md:flex-row w-[100%] justify-around align-middle'>
                 <div className='relative'>
-                  <h3 className='text-white text-2xl font-semibold'>
+                  <h3 className='text-white text-2xl md:text-2xl font-semibold text-center md:text-start'>
                     Contrate a melhor equipe de desenvolvedores e design do <br/>mundo,
                     eles se encontram aqui na Zero 27 Code!
                   </h3>
-                  <img className='absolute left-[48rem] bottom-9' src='/group47.png'/>
-                  <img className='absolute left-[39rem] top-24' src='/group48.png'/>
+                  <img className='absolute w-16 md:w-20 right-3 md:left-[48rem] top-44 md:bottom-9 md:block' src='/group47.png'/>
+                  <img className='absolute w-14 md:w-20 left-3 md:left-[39rem] md:top-24 md:block' src='/group48.png'/>
                 </div>
                 <div className='relative'>
-                  <img className='absolute left-48 bottom-14' src='/group46.png'/>
-                  <button className='bg-[#EF0178] text-white p-5'>ENTRAR EM CONTATO</button>
+                  <img className='absolute left-48 bottom-14 hidden md:first-letter:block' src='/group46.png'/>
+                  <button className='bg-[#EF0178] text-white p-5 md:p-5 mt-5 md:mt-0'>ENTRAR EM CONTATO</button>
                 </div>
                 
               </div>
@@ -199,38 +199,39 @@ export default function Home() {
       {/* RODAPÈ */}
       <footer className='bg-[#0C111F] mt-16 flex justify-center'>
         <div className='container relative'>
-          <div className='absolute right-28 top-48'>
+          <div className='absolute right-28 top-48 hidden md:block'>
             <img src='/group47.png'/>
           </div>
-          <div className='flex justify-center text-white flex-col items-center'>
-            <h3 className='text-3xl font-light pt-24'>Entre em contato conosco</h3>
-            <p className='font-light pt-6 text-gray-400'>Ao entrar em contato, você aceita nossa política de privacidade.</p>
-            <div className=' flex justify-center w-[100%] pb-24 pt-8'>
-              <div className='flex flex-col w-[30%] mx-3'>
-                  <input type='text' name='nome' placeholder='Nome' className='bg-slate-800 text-white p-4 w-auto my-2'/>
-                  <input type='text' name='telefone' placeholder='Telefone' className='bg-slate-800 text-white p-4 w-auto my-2'/>
-                  <input type='text' name='email' placeholder='E-mail' className='bg-slate-800 text-white p-4 w-auto my-2'/>
-                  <button className='bg-[#5243C2] p-4 my-2 text-white'>Enviar Mensagem</button>
+          <div className='flex justify-center text-white flex-col items-center pb-20'>
+            <h3 className='text-2xl md:text-3xl font-light pt-24'>Entre em contato conosco</h3>
+            <p className='text-sm md:text-base text-center font-light pt-6 text-gray-400'>Ao entrar em contato, você aceita nossa política de privacidade.</p>
+            <div className='flex flex-col md:flex-row justify-center md:w-[100%] pb-2 pt-8'>
+              <div className='flex flex-col md:flex-col items-center md:items-start md:w-[30%] mx-3'>
+                  <input type='text' name='nome' placeholder='Nome' className='bg-slate-800 text-white p-4 w-full my-2'/>
+                  <input type='text' name='telefone' placeholder='Telefone' className='bg-slate-800 text-white p-4 w-full my-2'/>
+                  <input type='text' name='email' placeholder='E-mail' className='bg-slate-800 text-white p-4 w-full my-2'/>
               </div>
-              <div className='flex w-[30%] mx-3 '>
-                <textarea name='mensagem' placeholder='Mensagem' className='bg-slate-800 w-[100%]'></textarea>
+              <div className='flex justify-center md:w-[30%] mx-3 '>
+                <textarea name='mensagem' placeholder='Mensagem' className='bg-slate-800 w-[100%] p-1 my-2'></textarea>
               </div>
             </div>
+            <button className='bg-[#5243C2] p-4 my-2 text-white'>Enviar Mensagem</button>
+            
           </div>
 
-          <div className='absolute bottom-44 left-12'>
+          <div className='absolute bottom-44 left-12 hidden md:block'>
             <img src='/group48.png'/>
           </div>
 
-          <div className='flex justify-between items-center w-[100%] pb-10'>
+          <div className='flex justify-center items-center w-[100%] pb-10'>
             <img src='/logo_027.png' width={180}/>
-            <ul className='list-none flex align-middle justify-center text-white'>
+            {/* <ul className='list-none flex align-middle justify-center text-white'>
               <li className='inline p-3'>Inicio</li>
               <li className='inline p-3'>Sobre Nós</li>
               <li className='inline p-3'>Serviços</li>
               <li className='inline p-3'>Preços</li>
               <li className='inline p-3'>Depoimentos</li>
-            </ul>
+            </ul> */}
           </div>
         </div>
         
