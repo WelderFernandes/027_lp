4fee58c967227c7cfcd412a9c4b1a367347a8741
Lucas Ramos Marques <lucas@rdweb.com.br>
2023-05-12 14:21:36 -0300

    feat: menu mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,20 @@
-import Image from 'next/image'
+"use client";
+import { Fragment } from 'react'
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Início', href: '#', current: true },
+  { name: 'Sobre Nós', href: '#', current: false },
+  { name: 'Serviços', href: '#', current: false },
+  { name: 'Preços', href: '#', current: false },
+  { name: 'Depoimentos', href: '#', current: false },
+]
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
 
 export default function Home() {
   return (
@@ -6,22 +22,82 @@ export default function Home() {
       <div className='flex justify-center'>
         <div className='container'>
           {/* MENU */}
-          <header className='flex justify-center align-middle justify-between mt-5'>
-            <img
-              src='/logo_027.png'
-              alt='Logo'
-              width={220}
-            />
+          <header className='flex justify-center align-middle mt-5'>
+            <Disclosure as="nav" className="w-[100%]">
+              {({ open }) => (
+                <>
+                  <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <div className="flex h-16 items-center justify-between">
+                      <div className="flex items-center">
+                        <div className="flex-shrink-0">
+                          <img
+                            className='w-52'
+                            src="/logo_027.png"
+                            alt="Zero 27 Code"
+                          />
+                        </div>
+                      </div>
+                      <div className="flex items-center">
+                        <div className="hidden md:block">
+                          <div className="ml-10 flex items-center space-x-4">
+                            {navigation.map((item) => (
+                              <a
+                                key={item.name}
+                                href={item.href}
+                                className={classNames(
+                                  item.current
+                                    ? 'text-[#EF0178]'
+                                    : 'text-black hover:text-[#EF0178]',
+                                  'rounded-md px-3 py-2 font-medium'
+                                )}
+                                aria-current={item.current ? 'page' : undefined}
+                              >
+                                {item.name}
+                              </a>
+                            ))}
+                          </div>
+                        </div>
+                      </div>
+                      <div className="hidden md:block">
+                        <div className="ml-4 flex items-center md:ml-6">
+                          <button className='border-[1px] rounded-md border-[#EF0178] px-8 py-3 text-[#EF0178] font-semibold hover:bg-[#EF0178] hover:text-white'>Contato</button>
+                        </div>
+                      </div>
+                      <div className="-mr-2 flex md:hidden">
+                        {/* Mobile menu button */}
+                        <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-[#EF0178] p-2 text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-[#EF0178]">
+                          <span className="sr-only">Open main menu</span>
+                          {open ? (
+                            <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                          ) : (
+                            <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                          )}
+                        </Disclosure.Button>
+                      </div>
+                    </div>
+                  </div>
 
-            <ul className='list-none flex align-middle justify-center'>
-              <li className='inline p-3 text-[#EF0178]'>Inicio</li>
-              <li className='inline p-3'>Sobre Nós</li>
-              <li className='inline p-3'>Serviços</li>
-              <li className='inline p-3'>Preços</li>
-              <li className='inline p-3'>Depoimentos</li>
-            </ul>
-
-            <button className='border-[1px] rounded-md border-[#EF0178] px-8 text-[#EF0178] font-semibold'>Contato</button>
+                  <Disclosure.Panel className="md:hidden">
+                    <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                      {navigation.map((item) => (
+                        <Disclosure.Button
+                          key={item.name}
+                          as="a"
+                          href={item.href}
+                          className={classNames(
+                            item.current ? 'bg-[#EF0178] text-white' : 'text-black hover:bg-[#EF0178] hover:text-white',
+                            'block rounded-md px-3 py-2 text-base font-medium'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </Disclosure.Button>
+                      ))}
+                    </div>
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
           </header>
 
           <main>
